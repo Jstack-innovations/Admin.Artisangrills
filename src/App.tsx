2a65fae3b979c708c8efd806be4d2e440a7872ac
JStack Innovations <wsamson630@gmail.com>
2026-03-20 21:41:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,6 @@ export default function PaidOrders() {
         <a href="/">All Orders</a>
         <a href="/users">Active Users</a>
         <a href="/tables">Available Tables</a>
-        <Link to="/tables">Available Tables</Link>
         <a href="/menu">Add Menu</a>
         <a href="/tax">Set Tax</a>
         <a href="/check-reservations">View Reservations</a>
@@ -158,8 +157,7 @@ export default function PaidOrders() {
             <p>{stats.totalPlaced}</p>
           </div>
 
-          <Link to="/tables">Available Tables</Link>
-
+        
           <div className="card">
             <h3>Total Served Orders</h3>
             <p>{stats.totalServed}</p>
