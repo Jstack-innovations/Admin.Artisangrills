8a06fbdca70c2901668b2b9dbd2a9b4ba7bcdb41
JStack Innovations <wsamson630@gmail.com>
2026-03-23 19:30:38 +0100

    Update AdminLogin.tsx

--- src/Admin/AdminLogin.tsx
+++ src/Admin/AdminLogin.tsx
@@ -30,7 +30,7 @@ export default function Login() {
   // Fetch admins
   useEffect(() => {
 
-    fetch(`${API_BASE}/adminnn`)
+    fetch(`${API_BASE}/admin`)
       .then(res => res.json())
       .then(data => {
         console.log("Admins fetched:",data);
