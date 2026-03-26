2e7bd1fc3d042baafa18a44dd8d55086b8e04488
JStack Innovations <wsamson630@gmail.com>
2026-03-26 19:52:21 +0100

    Update AddOrder.tsx

--- src/Admin/AddOrder.tsx
+++ src/Admin/AddOrder.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Css/AddOrder.css";
 import { API_BASE } from "../Config/api";
