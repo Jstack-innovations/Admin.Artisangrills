aaecbb33c9e3c347ef3fbd1cb5b9feb84b82a1a8
JStack Innovations <wsamson630@gmail.com>
2026-03-26 12:01:19 +0100

    Update api.tsx

--- src/Config/Utils/api.tsx
+++ src/Config/Utils/api.tsx
@@ -1,1 +1,23 @@
+import { API_BASE } from "../Config/api";
 
+export const apiFetch = async (
+  endpoint: string,
+  options: RequestInit = {}
+) => {
+  const response = await fetch(`${API_BASE}${endpoint}`, {
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+
+  // Auto handle unauthorized
+  if (response.status === 401) {
+    window.location.href = "/login";
+    return null;
+  }
+
+  return response;
+};
